881e58334a9e2681d30962005a42771f830db82c
Alien hunter no. 42 <84127070+alienHunterOnMars@users.noreply.github.com>
2021-08-02 12:54:34 +0530

    update

--- manifesto/src/lib.rs
+++ manifesto/src/lib.rs
@@ -2,5 +2,5 @@ pub mod contract;
 pub mod msg;
 pub mod state;
 
-#[cfg(target_arch = "wasm32")]
-cosmwasm_std::create_entry_points!(contract);
+// #[cfg(target_arch = "wasm32")]
+// cosmwasm_std::create_entry_points!(contract);
--- scripts/localterra_script.js
+++ scripts/localterra_script.js
@@ -1,23 +1,62 @@
 import {instantiateContract, uploadContract} from "./helpers.mjs";
 import {sign_manifesto, get_signeesCount, isSignee, get_signature} from "./manifesto_utils.js";
 import {LCDClient, LocalTerra, MnemonicKey} from "@terra-money/terra.js";
+import chalk from "chalk";
+import dotenv from "dotenv";
+import yargs from "yargs/yargs";
 
-async function main() {
 
-    // LOCAL TERRA INSTANCE
-    let terra = new LocalTerra();
-    let wallet = terra.wallets.test1;    
-    console.log("Wallet Address : " + wallet.key.accAddress )
+//----------------------------------------------------------------------------------------
+// Parse Input Parameters
+//----------------------------------------------------------------------------------------
+
+// Parse .env
+dotenv.config();
+
+// Parse options
+const argv = yargs(process.argv)
+  .options({
+    network: {
+      alias: "n",
+      type: "string",
+      demandOption: true,
+    }
+  })
+  .parseSync();
+
+  // LCD Client
+  let terra; 
+  if (!["columbus", "bombay"].includes(argv.network)) {
+    console.log(chalk.red("Error!"), "Invalid network: must be 'columbus' or 'bombay'");
+    process.exit(0);
+  } else {
+    terra =
+      argv.network == 
+      "columbus" ? new LCDClient({ URL: "https://lcd.terra.dev", chainID: "columbus-5" })
+       : 
+      new LCDClient({ URL: "https://bombay-lcd.terra.dev", chainID: "bombay-0008" });
+  
+    console.log(`\nNetwork  : ${chalk.cyan(argv.network)}`);
+  }
+
+  // WALLET FOR DEPLOYING
+  let deployer;
+  if (!process.env.MNEMONIC) {
+    console.log(chalk.red("Error!"), "MNEMONIC not provided");
+    process.exit(0);
+  } else {
+    deployer = terra.wallet( new MnemonicKey({mnemonic: process.env.MNEMONIC}) );
+    console.log(`Deployer : ${chalk.cyan(deployer.key.accAddress)}\n`);
+  }
   
     // MANIFESTO CONTRACT DEPLOYMENT
     let manifesto_id = await uploadContract(terra, wallet, '../manifesto/artifacts/manifesto_contract.wasm');
     console.log('MANIFESTO CONTRACT ID : ' + manifesto_id )
 
-    let manifesto_init_msg = {  }
+    let manifesto_init_msg = {}
     let manifesto_address = await instantiateContract(terra, wallet, manifesto_id, manifesto_init_msg );
 
     console.log('MANIFESTO ADDRESS : ' + manifesto_address )
-    // let manifesto_address = "terra18jeen6u5wygnxmlddxth6834mxnxjhgw9nvhep";
 
     // SIGN MANIFESTO TX
     let response = await sign_manifesto(terra, wallet, manifesto_address, "20 Leo, 11 BML", "24:59:59 MTC"); 
@@ -35,9 +74,9 @@ async function main() {
     if (signature_ && signature_.signee == wallet.key.accAddress) {
       console.log("SIGNATURE DETAILS : \n Signee : " + signature_.signee + " \n Martian Date : " + signature_.martian_date + " \n Martian Time : " + signature_.martian_time )
     } 
-  }
+  
 
 
 
-  main()
+  // main()
 
